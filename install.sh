9fa6383347f1c37b361f1690d5a615c0d8ffd755
Paul Kramme <paul99kramme@googlemail.com>
2016-05-18 20:52:23 +0200

    Rewrite install and update scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,27 @@
 #!bin/bash
+printf "ROVERPI INSTALL SCRIPT starting"
+printf "Installing nessesary dependecies...\n"
 apt-get update
 apt-get install ncurses-dev
 apt-get install git
 apt-get install clang
+printf "... done.\n"
 
+printf "Copying update_roverpi.sh to home folder\n"
 cp update_roverpi.sh /home/pi/
 
+printf "Cloning wiringPi...\n"
 cd
 git clone git://git.drogon.net/wiringPi
+cd wiringPi
+printf "... done.\n"
+printf "Building wiringPi...\n"
+./build
+printf "... done.\n"
 
+printf "Building roverpi...\n"
+cd
 cd roverpi/source
 ./compiler.sh
-
-cp roverpi /usr/bin/
+printf "... done.\n"
+printf "run\n\nsudo cp roverpi /usr/bin/\n\nto use 'sudo roverpi' to start roverpi."
--- update_roverpi.sh
+++ update_roverpi.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+printf "ROVERPI UPDATE SCRIPT started\n"
+
+printf "Deleting roverpi project file to provide clean build...\n"
 rm -rf roverpi
-rm /usr/bin/roverpi
+printf "... done.\n"
+
+printf "Cloning GitHub Repositority..."
 git clone git@192.168.247.12:vortexlab/roverpi.git
+printf "... done.\n"
+
+printf "Building roverpi...\n"
 cd roverpi/source
 ./compiler.sh
-cp roverpi /usr/bin/
+printf "... done.\n"
+cd
+printf "Run\n\nsudo rm /usr/bin/roverpi && sudo cp roverpi/source/roverpi /usr/bin\n\nto update executable."
