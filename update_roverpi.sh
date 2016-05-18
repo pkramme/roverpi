156104e35c00ec9ec563d66382805933f26f73b0
Paul Kramme <pjkramme@gmail.com>
2016-05-18 21:10:15 +0200

    Remove creapy git added marks

--- update_roverpi.sh
+++ update_roverpi.sh
@@ -12,7 +12,6 @@ printf "... done.\n"
 printf "Building roverpi...\n"
 rm /usr/bin/roverpi
 git clone git@github.com:paulkramme/roverpi.git
->>>>>>> master
 cd roverpi/source
 ./compiler.sh
 printf "... done.\n"
