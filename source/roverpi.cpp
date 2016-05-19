64351933352829b909d74172a71ddcfdfed53f85
Paul Kramme <pjkramme@gmail.com>
2016-05-19 21:04:45 +0200

    Add verbose mesg to thread start

--- source/roverpi.cpp
+++ source/roverpi.cpp
@@ -5,8 +5,9 @@ int main()
 	boost::thread_group tgroup;
 
 	tgroup.create_thread(boost::bind(&x_move));
+	std::cout << "LAUNCHED X_MOVE THREAD" << std::endl;
 	tgroup.create_thread(boost::bind(&input));
-
+	std::cout << "LAUNCHED INPUT THREAD" << std::endl;
 	tgroup.join_all();
 	std::cout << "ROVERPI SOFTWARE by Paul Kramme" << std::endl;
 	return 0;
