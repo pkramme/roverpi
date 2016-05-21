2ca0f0de8c4c3b00f99faa01041e49128dc32104
Paul Kramme <pjkramme@gmail.com>
2016-05-21 08:08:06 +0200

    Rework syntax in camservo

--- source/camservo_roverpi.ino
+++ source/camservo_roverpi.ino
@@ -18,11 +18,11 @@ void setup()
 	MS2.attach(3);
 	counter1 = 90;
 	counter2 = 90;
-	pinMode(7,INPUT);
-	pinMode(6,INPUT);
+	pinMode(7, INPUT);
+	pinMode(6, INPUT);
 
-	pinMode(9,INPUT);
-	pinMode(8,INPUT);
+	pinMode(9, INPUT);
+	pinMode(8, INPUT);
 	MS1.write(15);
 	MS2.write(15);
 	delay(300);
@@ -39,46 +39,46 @@ void loop()
 	MS1.write(counter1);
 	MS2.write(counter2);
 
-	if(digitalRead(7)==HIGH)
+	if(digitalRead(7) == HIGH)
 	{
 		counter1 = counter1 + schritte;
 		delay(del);
 	}
 
-	if(digitalRead(6)==HIGH)
+	if(digitalRead(6) == HIGH)
 	{
 		counter1 = counter1 - schritte;
 		delay(del);
 	}
 
-	if(digitalRead(9)==HIGH)
+	if(digitalRead(9) == HIGH)
 	{
 		counter2 = counter2 + schritte;
 		delay(del);
 	}
 
-	if(digitalRead(8)==HIGH)
+	if(digitalRead(8) == HIGH)
 	{
 		counter2 = counter2 - schritte;
 		delay(del);
 	}
 
-	if(counter1>170)
+	if(counter1 > 170)
 	{
 		counter1 = 170;
 	}
 
-	if(counter1<10)
+	if(counter1 < 10)
 	{
 		counter1 = 10;
 	}
 
-	if(counter2>170)
+	if(counter2 > 170)
 	{
 		counter2 = 170;
 	}
 
-	if(counter2<10)
+	if(counter2 < 10)
 	{
 		counter2 = 10;
 	}
