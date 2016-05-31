90255d7ae868ac8b24971702699c3d5cfe8ae8b2
Paul Kramme <pjkramme@gmail.com>
2016-05-31 11:35:59 +0200

    Set camstream timeout to ZERO

--- shellscripts/camstream.sh
+++ shellscripts/camstream.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-raspivid -o - -t 99999 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264
+raspivid -o - -t 0 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264
