3e2ac2acfceccc1523876e942153c2b28a509212
Paul Kramme <pjkramme@gmail.com>
2016-06-30 13:42:05 +0200

    Add long and short args

--- source/main.c
+++ source/main.c
@@ -6,26 +6,65 @@ int main(int argc, char *argv[])
 	printf("RoverPi Software v2\n");
 	if(argc != 2)
 	{
-		printf("USAGE: %s mode\n", argv[1]);
+		printf("USAGE: %s mode\n", argv[0]);
 		return 1;
 	}
 	else
 	{	
-		if(strcmp((argv)[1], "test") == 0)
+		/*
+		TEST MODE ARG LINK
+		*/
+		if(strcmp((argv)[1], "--test") == 0)
 		{
 			printf("MODE: TEST MODE\n");
 		}
-		else if(strcmp((argv)[1], "direct") == 0)
+		else if(strcmp((argv)[1], "-t") == 0)
+		{
+			printf("MODE: TEST MODE\n");
+		}
+
+		/*
+		DIRECT MODE ARG LINK
+		*/
+		else if(strcmp((argv)[1], "--direct") == 0)
 		{
 			printf("MODE: DIRECT MODE\n");
 		}
-		else if(strcmp((argv)[1], "headless") == 0)
+		else if(strcmp((argv)[1], "-d") == 0)
+		{
+			printf("MODE: DIRECT MODE\n");
+		}
+		
+		/*
+		HEADLESS MODE ARG LINK
+		*/
+		else if(strcmp((argv)[1], "--headless") == 0)
 		{
 			printf("MODE: HEADLESS MODE\n");
 		}
-		else if(strcmp((argv)[1], "remote") == 0)
+		else if(strcmp((argv)[1], "-h") == 0)
+		{
+			printf("MODE: HEADLESS MODE\n");
+		}
+
+		/*
+		REMOTE MODE ARG LINK
+		*/
+		else if(strcmp((argv)[1], "--remote") == 0)
 		{
 			printf("MODE: REMOTE MODE\n");
+		}
+		else if(strcmp((argv)[1], "-r") == 0)
+		{
+			printf("MODE: REMOTE MODE\n");
+		}
+
+		/*
+		HELP MESSAGE
+		*/
+		else if(strcmp((argv)[1], "--help") == 0)
+		{
+			printf("USAGE: %s mode\n\nDirect mode: Same as in v1\nHeadless mode: REPLACE\nRemote mode: REPLACE\nTest mode: REPLACE\n", argv[0]);
 		}
 		else
 		{
