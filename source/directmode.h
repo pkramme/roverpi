94abcc903b59f77d7a2435edc3ad2bc4fb92adc4
Paul Kramme <pjkramme@gmail.com>
2016-07-02 21:20:32 +0200

    Add structuring

--- source/directmode.h
+++ source/directmode.h
@@ -1,6 +1,39 @@
 #ifndef _DIRECTMODE_H_
 #define _DIRECTMODE_h_
 
-/*content here*/
+void screen_setup()
+{
+	//ncursesinit here!
+}
+
+void setup()
+{
+	//PINS HERE!
+}
+
+void forward
+{
+	//FORWARD
+}
+
+void backward()
+{
+	//BACKWARD
+}
+
+void left()
+{
+	//LEFT
+}
+
+void right()
+{
+	//RIGHT
+}
+
+void input()
+{
+	//INPUT LOOP HERE
+}
 
 #endif
