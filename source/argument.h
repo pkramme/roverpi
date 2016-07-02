4264a1b0d533a81175d374a305f1a37a97c965df
Paul Kramme <pjkramme@gmail.com>
2016-07-02 23:42:30 +0200

    Update argument.h

---
+++ source/argument.h
@@ -0,0 +1,13 @@
+#ifndef _ARGUMENT_H_
+#define _ARGUMENT_H_
+
+char arg_test[] = "--test";
+char arg_test_short[] = "-t";
+char arg_direct[] = "--direct";
+char arg_direct_short[] = "-d";
+char arg_headless[] = "--headless";
+char arg_headless_short[] = "-h";
+char arg_remote[] = "--remote";
+char arg_remote_short[] = "-r";
+
+#endif
