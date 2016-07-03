c4d7bf9c643909aa2b2b756b96d404acb1328e17
Paul Kramme <pjkramme@gmail.com>
2016-07-03 10:36:47 -0400

    Update getch.h

--- source/getch.h
+++ source/getch.h
@@ -2,60 +2,35 @@
 #define _GETCH_H_
 
 #include <termios.h>
+#include <unistd.h>
 #include <stdio.h>
 
-static struct termios old, new;
-
-/* Initialize new terminal i/o settings */
-void initTermios(int echo) 
+/* reads from keypress, doesn't echo */
+int getch(void)
 {
-	tcgetattr(0, &old); /* grab old terminal i/o settings */
-	new = old; /* make new settings same as old settings */
-	new.c_lflag &= ~ICANON; /* disable buffered i/o */
-	new.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-	tcsetattr(0, TCSANOW, &new); /* use these new terminal i/o settings now */
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
 }
 
-/* Restore old terminal i/o settings */
-void resetTermios(void) 
+/* reads from keypress, echoes */
+int getche(void)
 {
-	tcsetattr(0, TCSANOW, &old);
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
 }
 
-/* Read 1 character - echo defines echo mode */
-char getch_(int echo) 
-{
-	char ch;
-	initTermios(echo);
-	ch = getchar();
-	resetTermios();
-	return ch;
-}
-
-/* Read 1 character without echo */
-char getch(void) 
-{
-	return getch_(0);
-}
-
-/* Read 1 character with echo */
-char getche(void) 
-{
-	return getch_(1);
-}
-
-/* Let's test it out */
-/*
-int main(void)
-{
-	char c;
-	printf("(getche example) please type a letter: ");
-	c = getche();
-	printf("\nYou typed: %c\n", c);
-	printf("(getch example) please type a letter...");
-	c = getch();
-	printf("\nYou typed: %c\n", c);
-	return 0;
-}
-*/
-#endif 
+#endif
