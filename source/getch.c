cb2d05df7fbeb49e1adce6f9ee6707dd89c45362
Paul Kramme <pjkramme@gmail.com>
2016-07-05 16:02:06 +0200

    Update getch.c

--- source/getch.c
+++ source/getch.c
@@ -1,9 +1,21 @@
+/*
+Copyright (c) 2016 Paul Kramme
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
 #include<termios.h>
 #include<unistd.h>
 #include<stdio.h>
 #include"getch.h"
 
-/* reads from keypress, doesn't echo */
 int getch(void)
 {
 	struct termios oldattr, newattr;
@@ -17,7 +29,6 @@ int getch(void)
 	return ch;
 }
 
-/* reads from keypress, echoes */
 int getche(void)
 {
 	struct termios oldattr, newattr;
