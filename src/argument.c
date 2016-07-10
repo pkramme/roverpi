e241c1c96b8b291623836f619eeb03f156d95157
Paul Kramme <pjkramme@gmail.com>
2016-07-10 16:24:18 +0200

    Make args const

--- src/argument.c
+++ src/argument.c
@@ -21,18 +21,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
 #include"argument.h"
 
 char arg1[256];
-//char arg2[256];
-//char arg3[256];
 
-char arg_test[] = "--test";
-char arg_test_short[] = "-t";
-char arg_direct[] = "--direct";
-char arg_direct_short[] = "-d";
-char arg_headless[] = "--headless";
-char arg_headless_short[] = "-h";
-char arg_remote[] = "--remote";
-char arg_remote_short[] = "-r";
+const char arg_test[] = "--test";
+const char arg_test_short[] = "-t";
+const char arg_direct[] = "--direct";
+const char arg_direct_short[] = "-d";
+const char arg_headless[] = "--headless";
+const char arg_headless_short[] = "-h";
+const char arg_remote[] = "--remote";
+const char arg_remote_short[] = "-r";
 
--- src/argument.h
+++ src/argument.h
@@ -26,16 +26,14 @@ SOFTWARE.
 #define ARGUMENT_H_INCLUDED
 
 extern char arg1[256];
-extern char arg2[256];
-extern char arg3[256];
 
-extern char arg_test[];
-extern char arg_test_short[];
-extern char arg_direct[];
-extern char arg_direct_short[];
-extern char arg_headless[];
-extern char arg_headless_short[];
-extern char arg_remote[];
-extern char arg_remote_short[];
+extern const char arg_test[];
+extern const char arg_test_short[];
+extern const char arg_direct[];
+extern const char arg_direct_short[];
+extern const char arg_headless[];
+extern const char arg_headless_short[];
+extern const char arg_remote[];
+extern const char arg_remote_short[];
 
 #endif
