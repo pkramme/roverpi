09c9d9e4f66e943cb326ced34e90183b3d554a15
Paul Kramme <pjkramme@gmail.com>
2016-07-23 18:01:28 -0400

    Add more unit tests

--- src/unittest.c
+++ src/unittest.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+#include"unittest_move.h"
+
 int main(void)
 {
 	return 0;
--- src/unittest_move.c
+++ src/unittest_move.c
@@ -2,7 +2,7 @@
 #include"api/move.h"
 #include"api/init.h"
 
-int unittest_move(void)
+int unittest_move_forward(void)
 {
 	init(1);
 	if(forward_set(1))
@@ -15,12 +15,65 @@ int unittest_move(void)
 		puts("forward_set(0) failed.");
 		return 1;
 	}
-	if(!forward_set(83)) //MUST FAIL!
+	if(!forward_set(83)) /*MUST FAIL!*/
 	{
-		puts("forward_set(83) worked!");
 		return 1;
 	}
 	init(0);
-	puts("Unit test successfully finished.");
 	return 0;
+}
+
+int unittest_move_left(void)
+{
+	init(1);
+	if(left_set(1))
+	{
+		return 1;
+	}
+	if(left_set(1))
+	{
+		return 1;
+	}
+	if(!left_set(84)) /*Must fail*/
+	{
+		return 1;
+	}
+	init(0);
+	return 0;
+}
+
+int unittest_move_right(void)
+{
+	init(1);
+	if(right_set(1))
+	{
+		return 1;
+	}
+	if(right_set(0))
+	{
+		return 1;
+	}
+	if(!right_set(298))
+	{
+		return 1;
+	}
+	init(0);
+}
+
+int unittest_move_right(void)
+{
+	init(1);
+	if(backward_set(1))
+	{
+		return 1;
+	}
+	if(backward_set(0))
+	{
+		return 1;
+	}
+	if(!backward_set(9839))
+	{
+		return 1;
+	}
+	init(0);
 }
