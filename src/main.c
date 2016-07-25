3c8b78625525410822a1233c611de2278ff3d718
Paul Kramme <pjkramme@gmail.com>
2016-07-25 19:34:06 -0400

    Fix blankline

--- src/main.c
+++ src/main.c
@@ -49,7 +49,6 @@ int main(int argc, char *argv[])
 	puts("(C) 2016 Paul Kramme\nDistributed under MIT License");
 	puts("Read License in LICENSE.md or at");
 	puts("https://opensource.org/license/MIT");
-				
 #ifdef DEBUG
 	puts(" ______   _______  _______  __   __  _______  __   __  _______  ______   _______ ");
 	puts("|      | |       ||  _    ||  | |  ||       ||  |_|  ||       ||      | |       |");
