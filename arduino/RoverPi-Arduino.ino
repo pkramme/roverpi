6d8290795d2442360d2c8e71d3a8889f2fb7c78d
Paul Kramme <pjkramme@gmail.com>
2016-11-01 22:02:21 +0100

    Fix code formatting

--- arduino/RoverPi-Arduino.ino
+++ arduino/RoverPi-Arduino.ino
@@ -34,7 +34,6 @@ const int inbackward = 49;
 const int inleft = 47;
 const int inright = 45;
 
-
 const int reset = 42;
 
 const int inlight = 40;
@@ -48,194 +47,192 @@ const int outbackward = 8;
 const int outleft = 7;
 const int outright = 6;
 
-
-
-
 int forwardstat = 0;
 int backwardstat = 0;
 int leftstat = 0;
 int rightstat = 0;
 
-
 float lightstat = 0;
 
 
-
-
-void setup(){
-  Serial.begin(19200); //For Serial communication
- 
-  servosettings();
-
-  pinsettings();
-  
-  drivekill();
-
+void setup()
+{
+     Serial.begin(19200); //For Serial communication
+     servosettings();
+     pinsettings();
+     drivekill();
 }
 
-void loop(){
-  debug();
-  servoloop();
-  driveloop();
-  lightloop();
+void loop()
+{
+     debug();
+     servoloop();
+     driveloop();
+     lightloop();
 
-
-  ifreset();
-  delay(del);
-  ifreset();
+     ifreset();
+     delay(del);
+     ifreset();
 }
 
-void ifreset(){
-  if(digitalRead(reset) == HIGH)
-  {
-    servosetup();
-  }
+void ifreset()
+{
+     if(digitalRead(reset) == HIGH)
+     {
+          servosetup();
+     }
 }
 
-void servosetup(){
-      while((counterx != 90) || (countery != 90))
-    {
-      if(counterx < 90)
-      {
-        counterx++;
-      }
 
-      if(counterx > 90)
-      {
-        counterx--;
-      }
+void servosetup()
+{
+     while((counterx != 90) || (countery != 90))
+     {
+          if(counterx < 90)
+          {
+               counterx++;
+          }
 
-      if(countery < 90)
-      {
-        countery++;
-      }
+          if(counterx > 90)
+          {
+               counterx--;
+          }
 
-      if(countery > 90)
-      {
-        countery--;
-      }
+          if(countery < 90)
+          {
+               countery++;
+          }
 
-      MSx.write(counterx);
-      MSy.write(countery);
-      delay(del);
-    }
+          if(countery > 90)
+          {
+               countery--;
+          }
+          MSx.write(counterx);
+          MSy.write(countery);
+          delay(del);
+     }
 }
 
-void servotest(){
- //Servotest
- MSx.write(15);
- MSy.write(15);
- delay(500);
- MSx.write(160);
- MSy.write(160);
- delay(500);
- 
+
+void servotest()
+{
+     //Servotest
+     MSx.write(15);
+     MSy.write(15);
+     delay(500);
+     MSx.write(160);
+     MSy.write(160);
+     delay(500);
 }
 
-void servoloop(){
 
-  MSx.write(counterx);
-  MSy.write(countery);
+void servoloop()
+{
+     MSx.write(counterx);
+     MSy.write(countery);
 
-  
-  if(digitalRead(servoup) == HIGH)
-  {
-    counterx += steps;
-  }
+     if(digitalRead(servoup) == HIGH)
+     {
+          counterx += steps;
+     }
 
-  if(digitalRead(servodown) == HIGH)
-  {
-    counterx -= steps;
-    }
+     if(digitalRead(servodown) == HIGH)
+     {
+          counterx -= steps;
+     }
 
-  if(digitalRead(servoleft) == HIGH)
-  {
-    countery += steps;
-  }
+     if(digitalRead(servoleft) == HIGH)
+     {
+          countery += steps;
+     }
 
-  if(digitalRead(servoright) == HIGH)
-  {
-    countery -= steps;
-  }
+     if(digitalRead(servoright) == HIGH)
+     {
+          countery -= steps;
+     }
 
-  if(counterx > axismaxx)
-  {
-    counterx = axismaxx;
-  }
+     if(counterx > axismaxx)
+     {
+          counterx = axismaxx;
+     }
 
-  if(counterx < axisminx)
-  {
-    counterx = axisminx;
-  }
+     if(counterx < axisminx)
+     {
+          counterx = axisminx;
+     }
 
-  if(countery > axismaxy)
-  {
-    countery = axismaxy;
-  }
+     if(countery > axismaxy)
+     {
+          countery = axismaxy;
+     }
 
-  if(countery < axisminy)
-  {
-    countery = axisminy;
-  }
+     if(countery < axisminy)
+     {
+          countery = axisminy;
+     }
 }
 
-void driveloop(){
-  analogWrite(outforward,forwardstat);
-  analogWrite(outbackward,backwardstat);
-  analogWrite(outleft,leftstat);
-  analogWrite(outright,rightstat);
 
-  if((digitalRead(inforward)+digitalRead(inbackward)+digitalRead(inleft)+digitalRead(inright))>=2)
- {
-  drivekill();
-  delay(100);
-  return;
- }
- 
-  if(digitalRead(inforward)== HIGH){
-    backwardstat = 0;
-    leftstat = 0;
-    rightstat = 0;
-    delay(1);
+void driveloop()
+{
+     analogWrite(outforward,forwardstat);
+     analogWrite(outbackward,backwardstat);
+     analogWrite(outleft,leftstat);
+     analogWrite(outright,rightstat);
 
-    forwardstat=drivespeed;
-    
-    return;
-  }
-    if(digitalRead(inbackward)== HIGH){
-    forwardstat = 0;
-    leftstat = 0;
-    rightstat = 0;
-    delay(1);
-    
-    backwardstat=drivespeed;
-    
-    return;
-    }
+     if((digitalRead(inforward)+digitalRead(inbackward)+digitalRead(inleft)+digitalRead(inright))>=2)
+     {
+          drivekill();
+          delay(100);
+          return 0;
+     }
 
-    if(digitalRead(inleft)== HIGH){
-    forwardstat = 0;
-    backwardstat = 0;
-    rightstat = 0;
-    delay(1);
-    
-    leftstat=drivespeed;
-    
-    return;
-    }
+     if(digitalRead(inforward)== HIGH)
+     {
+          backwardstat = 0;
+          leftstat = 0;
+          rightstat = 0;
+          delay(1);
 
-    if(digitalRead(inright)== HIGH){
-    forwardstat = 0;
-    backwardstat = 0;
-    leftstat = 0;
-    delay(1);
-    
-    rightstat=drivespeed;
-    
-    return;
-    }
-    else{drivekill();}
-  }
+          forwardstat=drivespeed;
+
+          return 0;
+     }
+     if(digitalRead(inbackward)== HIGH)
+     {
+          forwardstat = 0;
+          leftstat = 0;
+          rightstat = 0;
+          delay(1);
+          backwardstat=drivespeed;
+          return 0;
+     }
+
+     if(digitalRead(inleft)== HIGH)
+     {
+          forwardstat = 0;
+          backwardstat = 0;
+          rightstat = 0;
+          delay(1);
+
+          leftstat=drivespeed;
+
+          return 0;
+     }
+
+     if(digitalRead(inright)== HIGH)
+     {
+          forwardstat = 0;
+          backwardstat = 0;
+          leftstat = 0;
+          delay(1);
+
+          rightstat=drivespeed;
+
+          return 0;
+     }
+     else{drivekill();}
+}
 
 void drivekill(){
     forwardstat = 0;
@@ -250,7 +247,7 @@ void drivekill(){
 }
 
 void lightloop(){
-  
+
   if(digitalRead(inlight)==HIGH){
     lightup();
     }
@@ -261,42 +258,42 @@ void lightloop(){
 }
 
 void debug(){
-  Serial.print("F: "); 
-  Serial.print(digitalRead(inforward));  
+  Serial.print("F: ");
+  Serial.print(digitalRead(inforward));
   Serial.print("  ");
   //Serial.print(forwardstat);
   Serial.print("\t");
-  
-  Serial.print("B: "); 
-  Serial.print(digitalRead(inbackward)); 
+
+  Serial.print("B: ");
+  Serial.print(digitalRead(inbackward));
   Serial.print("  ");
-  //Serial.print(backwardstat); 
+  //Serial.print(backwardstat);
   Serial.print("\t");
-  
-  Serial.print("L: "); 
-  Serial.print(digitalRead(inleft));  
+
+  Serial.print("L: ");
+  Serial.print(digitalRead(inleft));
   Serial.print("  ");
   //Serial.print(leftstat);
   Serial.print("\t");
-  
-  Serial.print("R: "); 
+
+  Serial.print("R: ");
   Serial.print(digitalRead(inright));
   Serial.print("  ");
-  //Serial.print(rightstat);  
+  //Serial.print(rightstat);
   Serial.print("\t");
-  
-  Serial.print("GE: "); 
+
+  Serial.print("GE: ");
   Serial.print((digitalRead(inforward)+digitalRead(inbackward)+digitalRead(inleft)+digitalRead(inright)));
   Serial.print("\t");
 
-  Serial.print("Light: "); 
+  Serial.print("Light: ");
   Serial.print(digitalRead(inlight));
   Serial.print("  ");
-  Serial.print(lightstat);  
+  Serial.print(lightstat);
   Serial.print("\t");
-  
+
   Serial.print("\n");
-  
+
 }
 
 void servosettings(){
@@ -309,7 +306,7 @@ void servosettings(){
 }
 
 void lightup(){
-  
+
   if(lightstat<brightness){
     lightstat = lightstat + 5;
     lightstat = lightstat * 1.3;
@@ -338,12 +335,12 @@ void pinsettings(){
   pinMode(inbackward,INPUT);
   pinMode(inleft,INPUT);
   pinMode(inright,INPUT);
-  
+
   pinMode(outforward,OUTPUT);
   pinMode(outbackward,OUTPUT);
   pinMode(outleft,OUTPUT);
   pinMode(outright,OUTPUT);
-  
+
   pinMode(inlight,INPUT);
   pinMode(outlight,OUTPUT);
 }
@@ -401,6 +398,3 @@ void racceleration(){
    }
 }
 */
-
-
-
